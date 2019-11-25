9a808031442facb7488555ff2cd6d2786a7a985f
Bry Cox <carl.cc@gmail.com>
2019-11-25 14:28:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function findMatching(drivers, name) {
   return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
 };
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(driver => driver[0].toLowerCase() === name[0].toLowerCase());
+};
